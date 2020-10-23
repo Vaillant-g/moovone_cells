5f84baebfcb758ac68d6d46f7e08d2b9e87f68a0
gautier <gautier.vaillant@epitech.eu>
2020-10-23 02:17:11 +0200

    ajout bonus react

---
+++ cellsReact.js
@@ -0,0 +1,52 @@
+export function cellsReact(initialConfiguration, k) {
+    var config = initialConfiguration;
+    var simulation = [];
+    simulation.push(config);
+    for (let cpt = 0; cpt < k; cpt++) {
+        config = tourSuivant(config);
+        simulation.push(config);
+    }
+    return simulation;
+}
+
+export function tourSuivant(config) {
+    var n = config.length;
+    var res = []; // config au tour suivant
+    var left, right;
+    for (let cpt = 0; cpt < n; cpt++) {
+        let newcell;
+        left = left = ((cpt === 0) ? (n - 1) : (cpt - 1));
+        right = right = ((cpt === n - 1) ? 0 : (cpt + 1));
+        // cas ou la cellule reçoit 2 signaux et passe à quiet
+        if (config[left] === '1' && config[right] === '1') {
+            newcell = '0';
+        }
+        else {
+            // cas ou la cellule est en repos
+            if (config[cpt] === '0') {
+                if ((config[left] === '1' && config[right] === '0') || (config[left] === '0' && config[right] === '1')) {
+                    newcell = '1';// cellule devient excitée
+                }
+                else if (config[left] === config[right] === '0') {
+                    newcell = config[cpt];
+                }
+                else
+                    newcell = '0'
+            }
+            else { // la cellule est excitée 
+                if (config[left] === '0' && config[right] === '0') {
+                    newcell = '0';
+                }
+                else {
+                    newcell = '1';
+                }
+            }
+        }
+        res.push(newcell);
+
+    }
+    return res;
+}
+
+cellsReact(['0', '1', '1', '0', '0', '1', '0'], 2);
+//cells(['0', '1', '1', '0', '0', '1', '0'], 2);
